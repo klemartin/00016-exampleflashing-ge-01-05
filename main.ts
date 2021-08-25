9b76c37667bcacdc18d744fd897b7c30cbd80be9
klemartin <55308390+klemartin@users.noreply.github.com>
2021-08-25 19:00:21 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+/**
+ * https://makecode.microbit.org/_HDW8pLXdsb9H
+ */
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    basic.pause(500)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    basic.pause(500)
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    basic.pause(500)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    basic.pause(500)
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    basic.pause(500)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    basic.pause(500)
+})
